45200d16f8e85092b9d3ea987de7c82ac2c84c5e
Vivek <vivek.ratnaparkhi10@gmail.com>
2018-08-09 00:19:01 +0530

    daily coding problem 4

--- problem_#4.py
+++ problem_#4.py
@@ -10,3 +10,28 @@ For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should g
 You can modify the input array in-place.
 """
 
+
+def func(data):
+	min_positive = 1
+	min_num = min([i for i in data if i>0], default=1)
+	
+	if min_num == min_positive:
+		for n in range(min_num, max(data)+1):
+			if n not in data:
+				return n
+		else:
+			return max(data)+1
+	else:
+		for n in range(1, max(data)+1):
+			if n not in data:
+				return n
+		else:
+			return max(data)+1
+
+
+print(func([3, 4, -1, 1]))
+print(func([1, 2, 0]))
+print(func([1, 1, 1, 2, 0]))
+print(func([-1, -2, 0, -5, -2]))
+
+# Note: This is not optimal solution.
